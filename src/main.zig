1e51dc2b4567c9273e54d06323517a806899b747
Cyprian Neugebauer <cyprianneugebauer28@gmail.com>
2025-08-03 20:02:04 +0200

    Add formatting errors and arena allocator per request

--- src/main.zig
+++ src/main.zig
@@ -37,11 +37,13 @@ const Command = enum {
     }
 };
 
-const NULL_STRING = "$-1\r\n";
+const NULL_BULK_STRING = "$-1\r\n";
 const PONG_MESSAGE = "+PONG\r\n";
 const OK_MESSAGE = "+OK\r\n";
 
 const ERR_ARG_NUM = "ERR wrong number of arguments";
+const ERR_SYNTAX = "ERR syntax error";
+const ERR_NOT_INTEGER = "ERR value is not an integer or out of range";
 
 fn formatSimpleString(allocator: std.mem.Allocator, string: []const u8) ![]const u8 {
     return std.fmt.allocPrint(allocator, "+{s}\r\n", .{string});
@@ -68,6 +70,10 @@ fn formatStringArray(allocator: std.mem.Allocator, strings: []const []const u8) 
     return buffer.toOwnedSlice();
 }
 
+fn formatError(allocator: std.mem.Allocator, message: []const u8) ![]const u8 {
+    return std.fmt.allocPrint(allocator, "-{s}\r\n", .{message});
+}
+
 fn calculateExpiration(ttl_ms: u64) i64 {
     const current_micros = std.time.microTimestamp();
     const ttl_us: i64 = @intCast(ttl_ms * std.time.us_per_ms);
@@ -75,38 +81,38 @@ fn calculateExpiration(ttl_ms: u64) i64 {
 }
 
 fn handleEcho(allocator: std.mem.Allocator, args: [64]?[]const u8) ![]const u8 {
-    const content = args[1] orelse return allocator.dupe(u8, NULL_STRING);
+    const content = args[1] orelse return NULL_BULK_STRING;
     return try formatBulkString(allocator, content);
 }
 
 fn handleGet(allocator: std.mem.Allocator, store: *Store, args: [64]?[]const u8) ![]const u8 {
-    const key = args[1] orelse return formatSimpleString(allocator, ERR_ARG_NUM);
+    const key = args[1] orelse return formatError(allocator, ERR_ARG_NUM);
 
     if (store.get(key)) |value_data| {
         return formatBulkString(allocator, value_data);
     } else {
-        return allocator.dupe(u8, NULL_STRING);
+        return NULL_BULK_STRING;
     }
 }
 
 fn handleSet(allocator: std.mem.Allocator, store: *Store, args: [64]?[]const u8) ![]const u8 {
-    const key = args[1] orelse return formatSimpleString(allocator, ERR_ARG_NUM);
-    const value_slice = args[2] orelse return formatSimpleString(allocator, ERR_ARG_NUM);
+    const key = args[1] orelse return formatError(allocator, ERR_ARG_NUM);
+    const value_slice = args[2] orelse return formatError(allocator, ERR_ARG_NUM);
     const px_command = args[3] orelse "";
 
     var expiration_us: ?i64 = null;
     if (std.ascii.eqlIgnoreCase(px_command, "PX")) {
-        const px_value_slice = args[4] orelse return formatSimpleString(allocator, "ERR syntax error");
-        const px_value_ms = std.fmt.parseInt(u64, px_value_slice, 10) catch return formatSimpleString(allocator, "ERR value is not an integer or out of range");
+        const px_value_slice = args[4] orelse return formatError(allocator, ERR_SYNTAX);
+        const px_value_ms = std.fmt.parseInt(u64, px_value_slice, 10) catch return formatError(allocator, ERR_NOT_INTEGER);
         expiration_us = calculateExpiration(px_value_ms);
     }
 
     try store.set(key, value_slice, expiration_us);
-    return allocator.dupe(u8, OK_MESSAGE);
+    return OK_MESSAGE;
 }
 
 fn handleRpush(allocator: std.mem.Allocator, lists_store: *Lists, args: [64]?[]const u8) ![]const u8 {
-    const key = args[1] orelse return formatSimpleString(allocator, ERR_ARG_NUM);
+    const key = args[1] orelse return formatError(allocator, ERR_ARG_NUM);
 
     var length: u64 = 0;
     for (args[2..]) |value_opt| {
@@ -118,21 +124,21 @@ fn handleRpush(allocator: std.mem.Allocator, lists_store: *Lists, args: [64]?[]c
 }
 
 fn handleLrange(allocator: std.mem.Allocator, lists_store: *Lists, args: [64]?[]const u8) ![]const u8 {
-    const key = args[1] orelse return formatSimpleString(allocator, ERR_ARG_NUM);
-    const start_index_slice = args[2] orelse return formatSimpleString(allocator, ERR_ARG_NUM);
-    const end_index_slice = args[3] orelse return formatSimpleString(allocator, ERR_ARG_NUM);
+    const key = args[1] orelse return formatError(allocator, ERR_ARG_NUM);
+    const start_index_slice = args[2] orelse return formatError(allocator, ERR_ARG_NUM);
+    const end_index_slice = args[3] orelse return formatError(allocator, ERR_ARG_NUM);
 
-    const start_index = std.fmt.parseInt(i64, start_index_slice, 10) catch return formatSimpleString(allocator, "ERR value is not an integer or out of range");
-    const end_index = std.fmt.parseInt(i64, end_index_slice, 10) catch return formatSimpleString(allocator, "ERR value is not an integer or out of range");
+    const start_index = std.fmt.parseInt(i64, start_index_slice, 10) catch return formatError(allocator, ERR_NOT_INTEGER);
+    const end_index = std.fmt.parseInt(i64, end_index_slice, 10) catch return formatError(allocator, ERR_NOT_INTEGER);
 
     const range = lists_store.lrange(key, start_index, end_index);
     return formatStringArray(allocator, range);
 }
 
-fn processRequest(handler: *AppHandler, allocator: std.mem.Allocator, request_data: []const u8) ![]const u8 {
+fn processRequest(handler: *AppHandler, request_allocator: std.mem.Allocator, request_data: []const u8) ![]const u8 {
     const commands = try resp.RESP.parse(request_data);
 
-    var response_buffer = std.ArrayList(u8).init(allocator);
+    var response_buffer = std.ArrayList(u8).init(request_allocator);
 
     for (commands) |command_parts_optional| {
         const command_parts = command_parts_optional orelse continue;
@@ -142,14 +148,13 @@ fn processRequest(handler: *AppHandler, allocator: std.mem.Allocator, request_da
         const command = Command.fromSlice(command_str) orelse continue;
 
         const response = try switch (command) {
-            .PING => allocator.dupe(u8, PONG_MESSAGE),
-            .ECHO => handleEcho(allocator, command_parts),
-            .GET => handleGet(allocator, handler.store, command_parts),
-            .SET => handleSet(allocator, handler.store, command_parts),
-            .RPUSH => handleRpush(allocator, handler.lists, command_parts),
-            .LRANGE => handleLrange(allocator, handler.lists, command_parts),
+            .PING => PONG_MESSAGE,
+            .ECHO => handleEcho(request_allocator, command_parts),
+            .GET => handleGet(request_allocator, handler.store, command_parts),
+            .SET => handleSet(request_allocator, handler.store, command_parts),
+            .RPUSH => handleRpush(request_allocator, handler.lists, command_parts),
+            .LRANGE => handleLrange(request_allocator, handler.lists, command_parts),
         };
-        defer allocator.free(response);
 
         try response_buffer.writer().writeAll(response);
     }
@@ -158,17 +163,21 @@ fn processRequest(handler: *AppHandler, allocator: std.mem.Allocator, request_da
 }
 
 const AppHandler = struct {
-    allocator: std.mem.Allocator,
+    app_allocator: std.mem.Allocator,
     store: *Store,
     lists: *Lists,
 
     pub fn init(allocator: std.mem.Allocator, store: *Store, lists_store: *Lists) AppHandler {
-        return .{ .allocator = allocator, .store = store, .lists = lists_store };
+        return .{ .app_allocator = allocator, .store = store, .lists = lists_store };
     }
 
     pub fn handleRequest(self: *AppHandler, client_fd: posix.fd_t, request_data: []const u8) !void {
-        const response = try processRequest(self, self.allocator, request_data);
-        defer self.allocator.free(response);
+        var arena = std.heap.ArenaAllocator.init(self.app_allocator);
+        defer arena.deinit();
+
+        const request_allocator = arena.allocator();
+
+        const response = try processRequest(self, request_allocator, request_data);
 
         var client_file = std.fs.File{ .handle = client_fd };
         try client_file.writer().writeAll(response);
